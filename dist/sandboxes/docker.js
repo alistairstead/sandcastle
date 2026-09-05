d827a33c9eab00b2f81c618cedc96fd86bcd37b9
Alistair Stead <62936+alistairstead@users.noreply.github.com>
2026-09-05 21:32:59 +0100

    build: commit dist for direct git-dependency install

---
+++ dist/sandboxes/docker.js
@@ -0,0 +1,9 @@
+import { createRequire } from 'node:module';
+export { docker } from '../chunk-ZXCNAPSU.js';
+export { defaultImageName } from '../chunk-XBH46AQY.js';
+import '../chunk-BIWNFKGV.js';
+import '../chunk-NGBM7T3E.js';
+
+createRequire(import.meta.url);
+//# sourceMappingURL=docker.js.map
+//# sourceMappingURL=docker.js.map
